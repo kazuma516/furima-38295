8b62a6e5faa2eedf8f346af41033f2db000e36ef
kazuma516 <kizuma.9516@gmail.com>
2022-11-28 19:49:26 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,25 @@ class ItemsController < ApplicationController
   
   #before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
+    @item = Item.includes(:user).order('created_at DESC')
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+   else
+    render :new
+   end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :category_id, :item_condition_id, :prefecture_id, :preparation_day_id, :postage_type_id).merge(user_id: current_user.id)
+  end
 end
