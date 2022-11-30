24fe34916f552f4a98a1f941b91016ed321244f4
kazuma516 <kizuma.9516@gmail.com>
2022-11-30 22:43:06 +0900

    プルダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,38 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one    :oder
+  has_one    :order
+
+
+    belongs_to :category
+    belongs_to :prefecture
+    belongs_to :item_condition
+    belongs_to :postage_type
+    belongs_to :preparation_day
+
+
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_type_id
+    validates :prefecture_id
+    validates :shipping_date_id
+
+    validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 }
+end
+
+
+with_options numericality: { other_than: 0 } do
+  validates :category_id
+  validates :prefecture_id
+  validates :item_condition_id
+  validates :postage_type_id
+  validates :preparation_day_id
+end
 end
---
+++ spec/factories/create_active_storages.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :create_active_storage do
+    
+  end
+end
