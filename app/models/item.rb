f24842e94789480707e2b6d01149ec4196ba19a6
kazuma516 <kizuma.9516@gmail.com>
2022-12-05 20:33:19 +0900

    商品出品機能修⑥

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
   #has_one    :order
 
 
-    belongs_to :category
-    belongs_to :prefecture
-    belongs_to :item_condition
-    belongs_to :postage_type
-    belongs_to :preparation_day
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_condition
+  belongs_to :postage_type
+  belongs_to :preparation_day
 
 
 
@@ -25,15 +25,15 @@ class Item < ApplicationRecord
     validates :preparation_day_id
 
     validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 }
-end
+  end
 
     validates :price, numericality: { only_integer: true }
 
-with_options numericality: { other_than: 0 } do
-  validates :category_id
-  validates :prefecture_id
-  validates :item_condition_id
-  validates :postage_type_id
-  validates :preparation_day_id
-end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :item_condition_id
+    validates :postage_type_id
+    validates :preparation_day_id
+  end
 end
