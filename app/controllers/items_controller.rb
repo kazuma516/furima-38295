9e32a6dc691cc7b38ef5f9bc7ab17a776bbf6285
kazuma516 <kizuma.9516@gmail.com>
2022-12-13 19:58:14 +0900

    商品削除修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,8 @@ class ItemsController < ApplicationController
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
+    #else
+      #redirect_to root_path
     end
   end
 
