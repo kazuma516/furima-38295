6c7847195b64c09f9d973a3db947bfcc9dc58e62
kazuma516 <kizuma.9516@gmail.com>
2022-12-20 21:34:45 +0900

    商品購入修正②

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
-    ユーザーid = FactoryBot.create(:user)
-    商品id = FactoryBot.create(:item)
-    @order_form = FactoryBot.build(:order_form, user_id: ユーザーid, item_id: 商品id)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end   
 
   describe '配送先情報の保存' do
